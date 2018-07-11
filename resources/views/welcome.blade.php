acb81eb9463b87f90d31ee39d5d0e11144e93ab4
Eric Haberstroh <gpg@erixpage.de>
2018-07-11 14:22:38 +0200

    Preliminary home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,25 +69,21 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">Einloggen</a>
+                        <a href="{{ route('register') }}">Registrieren</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    BH2
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/pille1842/bh2">GitHub</a>
                 </div>
             </div>
         </div>
